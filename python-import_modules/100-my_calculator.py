2bd4ce0a4fefc4616878b5a57e32b9c3716d6f02
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-02-23 08:00:35 -0800

    t100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
-for i in range(-ord('z'), -(ord('a') - 1)):
-    if (i % 2) != 0:
-        i = i - (ord('A') - ord('a'))
-    print("{}".format(chr(-i)), end='')
+if __name__ == '__main__':
+    import sys
+    args_list = sys.argv[1:]
+    if len(args_list) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    from calculator_1 import add, sub, mul, div
+    a = int(args_list[0])
+    b = int(args_list[2])
+    if args_list[1] == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif args_list[1] == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif args_list[1] == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif args_list[1] == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
