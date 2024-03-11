9c2cba42f549593a39e26c9d5b96e86cd06d39d1
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-03-11 04:35:22 -0700

    t1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,6 +6,7 @@ Define a Square class
 class Square:
     """Square class defined by size"""
 
-    def __init__(self,size):
+
+    def __init__(self, size):
         """"Instantiation with size (no type/value verification)"""
-        self.__size=size
+        self._ _size=size
