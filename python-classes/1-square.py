1da2627a972209bf6f0d2eb6fa6af34464ad823a
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-03-11 04:09:53 -0700

    t1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Define class Square 
+"""
+
+class Square:
+    def __init__(self,size=""):
+        """Instantiation with size (no type/value verification)"""
+        self.size=size
