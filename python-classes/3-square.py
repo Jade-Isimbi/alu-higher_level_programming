4ecb1bc45e07ff16d26a0185e395a6b595478bd6
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-03-11 06:51:47 -0700

    t3

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a Square
+"""
+
+
+class Square:
+    """Square class defined by size"""
+
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+     
+     def area(self):
+         """ public instance method calculate and  returns the current square area"""
+         
+        return self.__size **2
