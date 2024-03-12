7ed0a9555e44e35b56b89a10309849eb6e64477e
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-03-12 06:44:39 -0700

    t6

---
+++ python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Defines Square
+"""
+
+
+class Square:
+    """Square class defined by size"""
+
+    @property
+    def size(self):
+        """retrieving size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the value of size, value must be an integer"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+        @property
+        def position(self):
+            """retrieving position(getter)"""
+            return self.__position
+
+        @position.setter
+        def position(self, value):
+            if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+
+    def __init__(self, size=0, position(0, 0)):
+        """Instantiation with optional size and position"""
+        self.size = size
+        self.position=position
+
+    def area(self):
+        """calculate and return the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
