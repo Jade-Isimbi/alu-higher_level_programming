013de97f4bc8129e42da14cf19935c49bd8d94b0
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-03-24 19:25:32 +0000

    tt

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,6 +1,5 @@
-!/usr/bin/python3
-"""
-Python script that sends a POST request to the URL and
+
+n script that sends a POST request to the URL and
 to an URL with the letter as a parameter
 """
 import requests
